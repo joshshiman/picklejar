a692122c05fc7eb7df8e3031b9ccdb4dbeec1079
Joshua Shiman <joshshiman@proton.me>
2025-01-25 17:54:14 -0500

    Update tests.py

--- backend/apps/tests.py
+++ backend/apps/tests.py
@@ -1,3 +1,6 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+# Create Hangout
+# curl -X POST -H "Content-Type: application/json" -d '{"name": "Weekend Getaway", "created_at": "2025-03-15T18:00:00"}' http://127.0.0.1:8000/create/ghgfgfggheg
