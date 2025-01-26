62f9e0fa6f81ccff43376887b37be8598fdc4b94
Joshua Shiman <joshshiman@proton.me>
2025-01-26 00:08:09 -0500

    allowed hosts vercel

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-o!uydxted2t(bpos*_ibp5jqjs9lngzzemoytv8=dp_hpfqtyz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", ".vercel.app"]
 
 
 # Application definition
